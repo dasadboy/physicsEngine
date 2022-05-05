eb60fc01a89161f57d9b0165c3e5de4b6efcb3ff
dasadboy <akd741@gmail.com>
2022-05-05 16:52:37 -0400

    added declarations

--- premake5.lua
+++ premake5.lua
@@ -2,12 +2,14 @@ workspace "physics-engine"
     architecture "x64"
     configurations { "Debug", "Release" } 
 
-project "physics-engine"  
+project "physics-engine"
+    location "./"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}/%{prj.name}"
     objdir "bin/%{cfg.buildcfg}/%{prj.name}/obj"
-    files { "**.h", "**.cpp" }
+    files { "./src/**.cpp", "./src/**.h" }
+    includedirs { "./include" }
 
     filter "system:windows"
         cppdialect "C++20"
@@ -20,4 +22,4 @@ project "physics-engine"
 
     filter "configurations:Release"
         defines { "NDEBUG" }
-        optimize "On" 
+        optimize "On"
