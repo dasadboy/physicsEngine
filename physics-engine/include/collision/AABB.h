75883284d6255888f71b0b7810b016a1b856f126
dasadboy <akd741@gmail.com>
2022-06-17 13:48:46 -0400

    calcMinDistanceSquared function in AABB class

--- physics-engine/include/collision/AABB.h
+++ physics-engine/include/collision/AABB.h
@@ -52,6 +52,26 @@ namespace physics
         inline void resize(const vector3f& min, const vector3f& max)
         {
             assert(min.x < max.x && min.y < max.y && min.z < max.z);
+            m_min = min;
+            m_max = max;
+        }
+
+        inline float calcMinDistanceSquared(const AABB& b)
+        {
+            const AABB& a = *this;
+
+            #define checkOverlap(d, a, b) ( !((a.m_max.d < b.m_min.d) || (a.m_min.d > b.m_max.d)) )
+            #define calc(d, a, b) ( std::abs( a.m_min.d - b.m_max.d )  )
+
+            // find min distance of each component ( 0 if overlapping on that component component)
+            float diffX = checkOverlap(x, a, b) * ( std::min( calc(x, a, b), calc(x, b, a) ) );
+            float diffY = checkOverlap(y, a, b) * ( std::min( calc(y, a, b), calc(y, b, a) ) );
+            float diffZ = checkOverlap(z, a, b) * ( std::min( calc(z, a, b), calc(z, b, a) ) );
+
+            #undef checkOverlap
+            #undef calc
+
+            return diffX * diffX + diffY * diffY + diffZ * diffZ;
         }
     };
 }
